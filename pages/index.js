31edb7e990757ba9abdc18e9264ef1f203c73f84
Hiuri Mendes <85714066+hiurimendes@users.noreply.github.com>
2023-07-27 17:27:33 +0000

    adiciona scripts `list:check` e `liist:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Thaise, eu amo você. Mulher da minha vida, minha esposa, minha vida. Se você me quer, dá uma piscadinha! 😘</h1>
+function Home() {
+  return (
+    <h1>
+      Thaise, eu amo você. Mulher da minha vida, minha esposa, minha vida. Se
+      você me quer, dá uma piscadinha! 😘
+    </h1>
+  );
 }
 
 export default Home;
