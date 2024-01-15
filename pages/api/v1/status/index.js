2cc0dd24ee87dba464cc1f35fc6b11ab4c05653d
Hiuri Mendes <85714066+hiurimendes@users.noreply.github.com>
2024-01-15 17:32:23 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const queryPostgresVersion = await database.query("SHOW server_version;");
+  const postgres_version = queryPostgresVersion.rows[0].server_version;
+
+  const queryPostgresMaxConnection = await database.query(
+    "SHOW max_connections;",
+  );
+  const postgresMaxConnection =
+    queryPostgresMaxConnection.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const queryPostgresActiveConnection = await database.query({
+    text: "select count(*)::int active_connections from pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const postgresActiveConnection =
+    queryPostgresActiveConnection.rows[0].active_connections;
+
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      version: postgres_version,
+      max_connections: parseInt(postgresMaxConnection),
+      postgres_actconn: postgresActiveConnection,
+    },
+  });
 }
 
 export default status;
