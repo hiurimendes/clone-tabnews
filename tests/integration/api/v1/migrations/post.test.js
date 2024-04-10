64bd636343eb866d0df1ea7f90cb722a7183819b
Hiuri Mendes <hiurimendes@gmail.com>
2024-04-10 12:56:17 -0300

    Aula: Dia 24 - Expandindo testes e refatorando implementação

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -7,14 +7,30 @@ async function cleanDatabase() {
 }
 
 test("POST to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-  expect(response.status).toBe(200);
+  expect(response1.status).toBe(201);
 
-  const responseBody = await response.json();
+  const response1Body = await response1.json();
 
-  console.log(responseBody);
+  console.log(response1Body);
 
-  expect(Array.isArray(responseBody)).toBe(true);
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
+
+  /**
+   * after
+   */
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Body = await response2.json();
+
+  console.log(response2Body);
+
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
 });
