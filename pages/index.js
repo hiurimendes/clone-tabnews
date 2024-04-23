df92dd4d156d3bb1829d455d2a662bffd93895a5
Hiuri Mendes <hiurimendes@gmail.com>
2024-04-23 13:33:47 -0300

    Landing page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 function Home() {
   console.log(process.env.NODE_ENV);
   return (
-    <h1>
-      H&T 💖🕊️
-    </h1>
+    <div style={{ 
+      display: 'flex',
+      flexDirection: 'column',
+      margin: 0,
+      padding: 0, 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '98vh', 
+      width: '100%' ,
+      borderRadius: '8px',
+      background: '#f4f4f5',
+      lineHeight: 0,
+    }}>
+      <h1 style={{ 
+        color: '#ef4444' ,
+        fontFamily: 'monospace',
+        fontSize: '3rem',
+      }}>
+        H&T = 💖🕊️
+      </h1>
+      <p style={{ 
+        fontFamily: 'monospace',
+        fontSize: '14px'
+      }}>
+        Created for Your glory.
+      </p>
+    </div>
   );
 }
 
