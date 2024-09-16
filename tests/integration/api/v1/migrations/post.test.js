beca2ed576cc12beacb986a895edc39ae6a58925
Hiuri Mendes <hiurimendes@gmail.com>
2024-09-16 15:32:42 -0300

    style: linting

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -9,9 +9,12 @@ describe("POST to /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("For the first time", async () => {
-        const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response1.status).toBe(201);
 
         const response1Body = await response1.json();
@@ -20,9 +23,12 @@ describe("POST to /api/v1/migrations", () => {
         expect(response1Body.length).toBeGreaterThan(0);
       });
       test("For the second time", async () => {
-        const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response2.status).toBe(200);
 
         const response2Body = await response2.json();
