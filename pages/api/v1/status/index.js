01dfe6e416529a53aefe2ea28507360753e829f0
Hiuri Mendes <hiurimendes@gmail.com>
2026-09-04 09:44:41 -0300

    refactor: explicitly return in `/api/v1/status` `GET` handler

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -52,5 +52,5 @@ async function getHandler(request, response) {
     statusObject,
   );
 
-  response.status(200).json(secureOutputValues);
+  return response.status(200).json(secureOutputValues);
 }
